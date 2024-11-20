7416225f0896134b80cb32bce92b8c2fa7dace71
Samuel Pliska <samuelpliska@Mac-mini.fritz.box>
2024-11-20 18:10:27 +0100

    Added requestmapping to controller

--- backend/src/main/java/controllers/PersonController.java
+++ backend/src/main/java/controllers/PersonController.java
@@ -5,6 +5,8 @@ import com.sun.org.slf4j.internal.LoggerFactory;
 import models.MobileDevice;
 import models.Person;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +14,7 @@ public class PersonController {
     Logger logger = LoggerFactory.getLogger(PersonController.class);
 
     @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(path="person",method = RequestMethod.GET)
     public Person get() {
         return new Person("12345", "Test", new MobileDevice("1234", "Smartphone", "1234"));
     }
