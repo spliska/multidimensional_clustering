48d4c573349ec1eba4f4a82dd048824131867d23
Samuel Pliska <samuelpliska@Mac-mini.fritz.box>
2025-02-17 23:54:24 +0100

    Added DB Graph service

--- backend/src/main/java/services/db/PersonService.java
+++ backend/src/main/java/services/db/PersonService.java
@@ -10,17 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class PersonService {
-    public Location[] getAllPersons(){
+    public Person[] getAllPersons(){
         ArrayList<Person> persons=new ArrayList<Person>();
         Connection connection = null;
         try {
             connection = DriverManager.
                     getConnection("jdbc:mysql://localhost:8889/clustering_db", "root", "root");
             try (Statement stmt = connection.createStatement()) {
-                      String selectSql = "SELECT * FROM location";
+                String selectSql = "SELECT * FROM person";
                 try (ResultSet resultSet = stmt.executeQuery(selectSql)) {
                     while (resultSet.next()) {
                         persons.add(new Person(
@@ -39,10 +40,39 @@ public class PersonService {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return persons.toArray(new Location[0]);
+        return persons.toArray(new Person[0]);
     }
 
-    public Person[] getPersonById(int id){
+    public HashMap<String,Person> getAllPersonsMap(){
+        HashMap<String,Person> persons=new HashMap<String,Person>();
+        Connection connection = null;
+        try {
+            connection = DriverManager.
+                    getConnection("jdbc:mysql://localhost:8889/clustering_db", "root", "root");
+            try (Statement stmt = connection.createStatement()) {
+                String selectSql = "SELECT * FROM person";
+                try (ResultSet resultSet = stmt.executeQuery(selectSql)) {
+                    while (resultSet.next()) {
+                        persons.put(resultSet.getString(1),new Person(
+                                        resultSet.getString(1),
+                                        resultSet.getString(2),
+                                        resultSet.getString(3),
+                                        resultSet.getString(4),
+                                        new MobileDeviceService().getMobileDeviceByPersonId(resultSet.getString(1))
+                                )
+                        );
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return persons;
+    }
+
+    public Person[] getPersonById(String id){
         ArrayList<Person> persons=new ArrayList<Person>();
         Connection connection = null;
         try {
